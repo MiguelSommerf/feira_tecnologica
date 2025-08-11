137177b0b50ff36430346a9f00dd35fb0b48bece
Lívia Amaral <125597286+Liviaamaralsales@users.noreply.github.com>
2025-08-11 16:56:45 -0300

    Update tela_avaliacao.php

--- views/tela_avaliacao.php
+++ views/tela_avaliacao.php
@@ -68,14 +68,15 @@ if (session_status() === PHP_SESSION_NONE) {
     </main>
 
     <div id="mySideMenu" class="side-menu">
-      <a href="javascript:void(0)" class="close-btn" onclick="closeMenu()">&times;</a>
-      <a href="#">Mapa</a>
-      <a href="TelaAvaliacao.html">Avaliação</a>
-      <a href="tela_projetos.php">Projetos</a>
-      <a href="TelaRanking.html">Ranking</a>
-      <a href="TelaCursos.html">Cursos</a>
-      <a href="TelaSobreEtec.html">Sobre a Etec</a>
-      <a href="TelaConfiguracoes.html">Configurações</a>
+        <a href="javascript:void(0)" class="close-btn" onclick="closeMenu()">&times;</a>
+        <a href="tela_mapa.php">Mapa</a>
+        <a href="tela_projetos.php">Projetos</a>
+        <a href="tela_ranking.php">Ranking</a>
+        <a href="tela_cursos.php">Cursos</a>
+        <a href="tela_sobreEtec.php">Sobre a Etec</a>
+        <?php if(isset($_SESSION['id'])): ?>
+        <a href="../back/logout.php" class="deslogar" id="deslogar" name="deslogar">Sair da Conta</a>
+        <?php endif; ?>
     </div>
 
     <script>
