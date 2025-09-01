9ee9b920f365e82b274aa3e4c7ba1d8583e52d3e
MiguelSommerf <miguel.sommerfeldluiz@gmail.com>
2025-09-01 16:46:26 -0300

    Reenviando as alterações que foram excluídas pelo merge.

--- admin/views/alterar_projeto.php
+++ admin/views/alterar_projeto.php
@@ -16,54 +16,6 @@ if (!isset($id)) {
     exit();
 }
 
-<<<<<<< HEAD
-=======
-# Altera os dados do projeto quando o form é enviado.
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $titulo = $_POST['titulo'];
-    $descricao = $_POST['descricao'];
-    $sala = $_POST['sala'];
-    $bloco = $_POST['bloco'];
-    $stand = $_POST['stand'];
-    $orientador = $_POST['orientador'];
-
-    $sqlVerificar = "SELECT " . TABELA_PROJETO['id'] . " FROM " 
-                    . TABELA_PROJETO['nome_tabela'] . " WHERE " 
-                    . TABELA_PROJETO['bloco'] . " = ? AND " 
-                    . TABELA_PROJETO['sala'] . " = ? AND " 
-                    . TABELA_PROJETO['stand'] . " = ?";
-
-    $stmtVerificar = $mysqli->prepare($sqlVerificar);
-    $stmtVerificar->bind_param("sii", $bloco, $sala, $stand);
-    $stmtVerificar->execute();
-    $stmtVerificar->store_result();
-
-    if ($stmtVerificar->num_rows > 0) {
-        echo "<script>alert('Já existe um projeto cadastrado nesse Stand da mesma Sala e Bloco!')</script>";
-        echo "<script>window.location.href='criar_projeto.php'</script>";
-        $stmtVerificar->close();
-        exit();
-    }
-    $stmtVerificar->close();
-
-    $queryAlterarProjeto = "UPDATE ". TABELA_PROJETO['nome_tabela'] . " SET " 
-                                    . TABELA_PROJETO['titulo'] . " = ?, " 
-                                    . TABELA_PROJETO['descricao'] . " = ?, " 
-                                    . TABELA_PROJETO['bloco'] . " = ?, " 
-                                    . TABELA_PROJETO['sala'] . " = ?, " 
-                                    . TABELA_PROJETO['stand'] . " = ?, " 
-                                    . TABELA_PROJETO['orientador'] . " = ? WHERE " 
-                                    . TABELA_PROJETO['id'] . " = ?";
-    $stmtAlterarProjeto = $mysqli->prepare($queryAlterarProjeto);
-    $stmtAlterarProjeto->bind_param("sssiisi", $titulo, $descricao, $bloco, $sala, $stand, $orientador, $id);
-    $stmtAlterarProjeto->execute();
-
-    echo "<script>alert('Projeto alterado com sucesso!')</script>";
-    echo "<script>window.location.href='projetos.php'</script>";
-    exit();
-}
-
->>>>>>> 5bfaab1dbdbe00de4c18360ccbfa3e5c96db3ab9
 # Pega os dados do projeto.
 $sqlSelecionarProjeto = "SELECT * FROM " . TABELA_PROJETO['nome_tabela'] . " WHERE " . TABELA_PROJETO['id'] . " = ?";
 $stmtSelecionarProjeto = $mysqli->prepare($sqlSelecionarProjeto);
--- admin/views/projetos.php
+++ admin/views/projetos.php
@@ -1,21 +1,21 @@
 <?php
-    require_once __DIR__ . '/../../config/database.php';
+require_once __DIR__ . '/../../config/database.php';
 
-    session_start();
-    if (!isset($_SESSION['admin']) or $_SESSION['admin'] !== 1) {
-        header('Location: ../views/home.php');
-        exit('Você não tem permissão para acessar esta página.');
-    }
+session_start();
+if (!isset($_SESSION['admin']) or $_SESSION['admin'] !== 1) {
+    header('Location: ../views/home.php');
+    exit('Você não tem permissão para acessar esta página.');
+}
 
-    # Esse SELECT é pra trazer todos os dados que podem ser alterados, dos projetos.
-    $sqlProjeto = "SELECT " . TABELA_PROJETO['id'] . ", " 
-                            . TABELA_PROJETO['titulo'] . ", " 
-                            . TABELA_PROJETO['sala'] . ", " 
-                            . TABELA_PROJETO['bloco'] . ", " 
-                            . TABELA_PROJETO['stand'] . " FROM " 
-                            . TABELA_PROJETO['nome_tabela'] . " ORDER BY " 
-                            . TABELA_PROJETO['id'] . " ASC";
-    $resultProjeto = $mysqli->query($sqlProjeto);
+# Esse SELECT é pra trazer todos os dados que podem ser alterados, dos projetos.
+$sqlProjeto = "SELECT " . TABELA_PROJETO['id'] . ", " 
+                        . TABELA_PROJETO['titulo'] . ", " 
+                        . TABELA_PROJETO['sala'] . ", " 
+                        . TABELA_PROJETO['bloco'] . ", " 
+                        . TABELA_PROJETO['stand'] . " FROM " 
+                        . TABELA_PROJETO['nome_tabela'] . " ORDER BY " 
+                        . TABELA_PROJETO['id'] . " ASC";
+$resultProjeto = $mysqli->query($sqlProjeto);
 
 ?>
 <!DOCTYPE html>
@@ -35,39 +35,17 @@
 
     <!-- Div para organizar os projetos em coluna, do primeiro ao último. -->
     <div class="container">
-        <h2>Projetos Cadastrados</h2>
-        <table>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Título</th>
-                    <th>Bloco</th>
-                    <th>Sala</th>
-                    <th>Stand</th>
-                    <th>Ações</th>
-                </tr>
-            </thead>
-            <tbody>
-                <!-- Todos vão ter o botão "editar", que vai mandar o admin para a tela alterar_projeto-->
-                <?php while ($row = $resultProjeto->fetch_assoc()): ?>
+        <?php if ($resultProjeto->num_rows > 0): ?>
+            <h2>Projetos Cadastrados</h2>
+            <table>
+                <thead>
                     <tr>
-                        <td><strong><?= htmlspecialchars($row['id_projeto']); ?></strong></td>
-                        <td><?= htmlspecialchars($row['titulo_projeto']); ?></td>
-                        <td><?= htmlspecialchars($row['bloco_projeto']); ?></td>
-                        <td><?= htmlspecialchars($row['sala_projeto']); ?></td>
-                        <td><?= htmlspecialchars($row['stand_projeto']); ?></td>
-                        <td>
-                            <div class="actions">
-                                <form action="alterar_projeto.php" method="GET">
-                                    <input type="hidden" name="id_projeto" value="<?= $row['id_projeto'] ?>">
-                                    <button type="submit">Editar</button>
-                                </form>
-                                <form method="POST">
-                                    <input type="hidden" name="id_projeto" value="<?= $row['id_projeto'] ?>">
-                                    <button class="btn-delete" type="submit" onclick="deleteProject()">Excluir</button>
-                                </form>
-                            </div>
-                        </td>
+                        <th>ID</th>
+                        <th>Título</th>
+                        <th>Sala</th>
+                        <th>Bloco</th>
+                        <th>Stand</th>
+                        <th>Ações</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -89,7 +67,9 @@
                             </td>
                         </tr>
                     <?php endwhile; ?>
-                <?php endwhile; ?>
+                <?php else: ?>
+                    <h2>Nenhum projeto registrado.</h2>
+                <?php endif; ?>
             </tbody>
         </table>
     </div>
