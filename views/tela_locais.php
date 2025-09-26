f06827ea9ba5774720108b59e72fef588e54092b
Enzo Móbile de Oliveira <prenzo0001@gmail.com>
2025-09-26 18:37:46 -0300

    Começando a tela_locais.

---
+++ views/tela_locais.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/tela_locais.css">
+    <title>Locais da escola</title>
+</head>
+<body>
+    <header class="headerLogo">
+        <img src="../assets/img/etecmcm.png" alt="EtecMCM">
+    </header>
+    <div class="conteudo">
+        <div class="separacao">
+            <p>Blocos</p>
+            <a href="#">Bloco A</a>
+            <a href="#">Bloco B</a>
+        </div>
+        <div class="separacao">
+            <p>Outros locais</p>
+            <a href="#">Biblioteca</a>
+            <a href="#">Auditório</a>
+            <a href="#">Quadra</a>
+        </div>
+        <h2>Locais</h2>
+    </div>
+</body>
+</html>
